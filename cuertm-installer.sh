95418a9dba4618102aed122c2cb74197ac52d7be
Pratik Tayade <pratik.tayade@cuelogic.co.in>
2016-08-16 14:40:55 +0530

    Update cuertm-installer.sh

--- cuertm-installer.sh
+++ cuertm-installer.sh
@@ -3,4 +3,4 @@
 INSTALL_PREFIX="/usr/local/bin"
 APP="https://raw.githubusercontent.com/pratikt-cuelogic/cue-rtm/master/cuertm"
 curl -OL "$APP"
-install -v -m 0644 cuertm "$INSTALL_PREFIX"
+install -v -m 0777 cuertm "$INSTALL_PREFIX"
